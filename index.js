e942f65ec0e5539435d384b694bef38358586c6b
Programming Hero <programming.hero1@gmail.com>
2022-04-17 19:00:57 -0500

    initial connection setup

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+
+// user:dbuser1
+// password:xLeDtsSERnlIPrJ3
+
+
+
+const uri = "mongodb+srv://dbuser1:xLeDtsSERnlIPrJ3@cluster0.vwx9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) =>{
+    res.send('Running My Node CRUD Server');
+});
+
+app.listen(port, () =>{
+    console.log('CRUD Server is running');
+})
